77ea19bd8a24f6010013f8e4b5b664dda56e6706
Chris Drane <csdrane@gmail.com>
2019-02-12 18:03:29 -0500

    Fix error in date-format

--- src/file_parser/date.clj
+++ src/file_parser/date.clj
@@ -2,7 +2,7 @@
   (:require [clj-time.core :as t]
             [clj-time.format :as f]))
 
-(def date-format (f/formatter t/utc "YYYY-MM-dd" "YYYY/MM/dd" "MM/dd/YYYY MM-dd/YYYY"))
+(def date-format (f/formatter t/utc "YYYY-MM-dd" "YYYY/MM/dd" "MM/dd/YYYY" "MM-dd-YYYY"))
 
 (defn date-parser [word]
   (f/parse date-format word))
