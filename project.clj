f7805634a6c6b1c56d5bb26566ff9fe5586cf86b
Chris Drane <csdrane@gmail.com>
2019-02-15 18:12:26 -0500

    Add CLI front-end

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.4.1"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [clj-time "0.15.0"]]
   :plugins [[quickie "0.4.2"]]
   :main ^:skip-aot file-parser.core
--- src/file_parser/core.clj
+++ src/file_parser/core.clj
@@ -1,7 +1,40 @@
 (ns file-parser.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]]
+            [file-parser.parse :as parse]
+            [file-parser.view :as view]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def cli-options
+  [["-c" "--comma-file FILE" "Parse comma-seperated file"]
+   ["-s" "--space-file FILE" "Parse space-seperated file"]
+   ["-p" "--pipe-file FILE" "Parse pipe-seperated file"]
+   ["-h" "--help"]])
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn validate-args [args]
+  (let [{:keys [options errors summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options) {:exit-message summary :ok? true}
+      errors {:exit-message (error-msg errors)}
+      :else {:options options})))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn start [{:keys [comma-file pipe-file space-file]}]
+  (let [comma-results (when comma-file (parse/parse-comma-file comma-file))
+        pipe-results  (when pipe-file (parse/parse-pipe-file pipe-file))
+        space-results  (when space-file (parse/parse-space-file space-file))
+        combined-results (concat '() comma-results pipe-results space-results)]
+    (view/view combined-results)))
+
+(defn -main [& args]
+  (let [{:keys [options exit-message ok?]} (validate-args args)]
+    (if exit-message
+      (exit (if ok? 0 1) exit-message)
+      (start options))))
